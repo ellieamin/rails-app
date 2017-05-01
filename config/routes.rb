2678229bdffbb536b872ed3ced56cd0708cfd6ea
Ellie <emotamedi90@gmail.com>
2017-05-01 18:07:11 +1000

    Created static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+root 'static_pages#home'
+get '/contact-us', to: 'static_pages#contact'
+get '/about-us', to: 'static_pages#about'
+get '/portfolio', to: 'static_pages#portfolio'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
